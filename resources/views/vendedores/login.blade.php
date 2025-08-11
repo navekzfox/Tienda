0c96cf39c5d2957b517504d8cc238401a602e5c4
Evan <nave0802alexander@gmail.com>
2025-08-11 16:55:14 -0600

    modif mini

--- resources/views/vendedores/login.blade.php
+++ resources/views/vendedores/login.blade.php
@@ -4,12 +4,20 @@
 <main class="formulario">
     <h2>Ingreso Vendedor</h2>
     <form action="{{ route('vendedores.login.submit') }}" method="POST">
+        @if ($errors->any())
+            <div class="Alerta-de-advertencia" role="alert">
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+                <button type="button" class="boton-de-cierre" onclick="cerrarAlerta(this)" aria-label="Cerrar">x</button>
+            </div>
+        @endif
         @csrf
             <label for="Clave_ingreso" class="form-label">Código de ingreso</label>
-            <input type="password" id="Clave_ingreso" name="Clave_ingreso" class="controls" value="{{ old('codigo') }}" required>
-            @error('Clave_ingreso')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
+            <input type="password" id="Clave_ingreso" name="Clave_ingreso" class="controls" value="{{ old('codigo') }}">
+
         <button class="botons">Ingresar</button>
     </form>
 
