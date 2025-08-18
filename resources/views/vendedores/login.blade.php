f9c3ea869dbbb3f5e3e30558cdc1defbce169357
Evan <nave0802alexander@gmail.com>
2025-08-18 13:42:38 -0600

    Modificación en el log-in
    se cambio para que no se pueda auto complementar

--- resources/views/vendedores/login.blade.php
+++ resources/views/vendedores/login.blade.php
@@ -3,7 +3,7 @@
 @section('contenido')
 <main class="formulario">
     <h2>Ingreso Vendedor</h2>
-    <form action="{{ route('vendedores.login.submit') }}" method="POST">
+    <form action="{{ route('vendedores.login.submit') }}" method="POST" autocomplete="off">
         @if ($errors->any())
             <div class="Alerta-de-advertencia" role="alert">
                 <ul>
@@ -16,7 +16,7 @@
         @endif
         @csrf
             <label for="Clave_ingreso" class="form-label">Código de ingreso</label>
-            <input type="password" id="Clave_ingreso" name="Clave_ingreso" class="controls" value="{{ old('codigo') }}" autocomplete="new-password">
+            <input type="password" id="Clave_ingreso" name="Clave_ingreso" class="controls" value="{{ old('codigo') }}">
 
         <button class="botons">Ingresar</button>
     </form>
